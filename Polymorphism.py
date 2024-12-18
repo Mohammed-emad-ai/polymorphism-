3b35f29ef106dd86e3b51c0d8b08f9418ddf4186
Mohammed emad <mohammedemad110200@gmail.com>
2024-12-19 00:07:19 +0200

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,29 @@
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+    def area(self):
+        pass
+    
+    def __str__(self):
+        return self.name
+class Square(Shape):
+    def __init__(self, length):
+        super().__init__("Square")
+        self.length = length
+
+    def area(self):
+        return self.length**2
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__("Circle")
+        self.radius = radius
+
+    def area(self):
+        return 3.14*self.radius**2
+
+a = Square(4)
+b = Circle(7)
+print(b)
+print(b.area())
